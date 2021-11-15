590e82f8f63afc14445a2eeb52fd193e23f57127
Shen PEI-RU <3a832056@gm.student.ncut.edu.tw>
2021-11-15 22:33:22 +0800

    初步建置三個路由(Routes)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/', function () {
+    //
+});
+Route::post('/task', function (Request $request) {
+    //
+});
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
